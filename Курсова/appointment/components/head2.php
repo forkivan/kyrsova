1251e67d37e5426bfc9dc610e00f0750263cc1d4
forkivan <diorg99@gmail.com>
2024-05-19 21:07:47 +0300

    9

---
+++ Курсова/appointment/components/head2.php
@@ -0,0 +1,6 @@
+<head>
+    <meta charset="UTF-8">
+    <title>Achillelius - Запис на прийом</title>
+    <link rel="stylesheet" href="../login-and-register/assets/pico.classless.jade.css">
+    <link rel="stylesheet" href="../login-and-register/assets/chose.css">
+</head>
