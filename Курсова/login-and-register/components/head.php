45d6f50d09a4d6f1c4f75862cf07c06e52152459
forkivan <diorg99@gmail.com>
2024-05-19 21:04:15 +0300

    4

--- Курсова/login-and-register/components/head.php
+++ Курсова/login-and-register/components/head.php
@@ -3,5 +3,4 @@
     <title>Achillelius - авторизація та реєстрація</title>
     <link rel="stylesheet" href="assets/pico.classless.jade.css">
     <link rel="stylesheet" href="assets/app.css">
-    <link rel="stylesheet" href="assets/style.css">
 </head>
