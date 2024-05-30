8b15a9f2454cfc10b5f0e411fffbcb792263860c
forkivan <diorg99@gmail.com>
2024-05-31 01:20:44 +0300

    sdvdsad

--- Курсова/header_login.php
+++ Курсова/header_login.php
@@ -25,9 +25,23 @@
 			</ul>
 		</nav>
 	</div>
+
+	<div class="MobileHeader">
+		<nav>
+			<ul>
+				<img src="\kyrsova\Курсова\Images\MainIcon.png" id="LogoHospital" alt="Logo icon"> 
+				<li><a class="ArrowButton" href="\kyrsova\Курсова\login-and-register\login.php">Вхід</a></li>
+				<li><a class="ArrowButton" href="\kyrsova\Курсова\index.php">Головна</a></li>
+				<li><a class="ArrowButton" href="\kyrsova\Курсова\aboutus\aboutus.php">Про нас</a></li>
+				<li><a class="ArrowButton" href="\kyrsova\Курсова\appointment\chose.php">Запис до лікаря</a></li>
+				<li><a class="ArrowButton" href="\kyrsova\Курсова\account\home.php">Кабінет</a></li>
+			</ul>
+		</nav>
+	</div>
 </html>
 
 
 <link rel="stylesheet" href="\kyrsova\Курсова\styles.css">
 <link rel="stylesheet" href="\kyrsova\Курсова\main_page_styles.css">
 <link rel="stylesheet" href="\kyrsova\Курсова\doctor_styles.css">
+<link rel="stylesheet" href="\kyrsova\Курсова\header.css">
